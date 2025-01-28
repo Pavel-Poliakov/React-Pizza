aee3ca436f032eac77c3c58e4c7c493c215f29fc
Pavel Polyakov <Polaykovp.s@gmail.com>
2025-01-28 10:06:52 +0300

    delete package.json

--- src/utils/calcCountItems.ts
+++
@@ -1,5 +0,0 @@
-import { CartItemProps } from '../@types/types';
-
-export const calcCountItems = (items: CartItemProps[]) => {
-	return items.reduce((sum, item) => sum + item.count, 0);
-};
--- src/utils/calcTotalPrice.ts
+++
@@ -1,7 +0,0 @@
-import { CartItemProps } from '../@types/types';
-
-export const calcTotalPrice = (items: CartItemProps[]) => {
-	return items.reduce((sum: number, obj: CartItemProps) => {
-		return sum + obj.price * obj.count;
-	}, 0);
-};
